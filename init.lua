009a271d96a68fd16bd7a8f8e79d584a92df4ae3
Norbert Zehetmayer <norbert.zehetmayer@gmail.com>
2026-02-16 14:43:24 +0100

    Turn on Nordfont and relativenumber, set tabstop, add Julia lsp

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -140,6 +140,11 @@ vim.o.timeoutlen = 300
 -- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
+
+-- Cinfigure tabstop
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+--vim.o.noexpandtab = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -670,6 +675,19 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      -- Julia LSP: mason-lspconfig v2 is broken for julials, so configure manually
+      if vim.fn.executable 'julia' == 1 then
+        if vim.fn.has 'nvim-0.11' == 1 then
+          vim.lsp.config('julials', {
+            capabilities = capabilities,
+          })
+          vim.lsp.enable 'julials'
+        else
+          require('lspconfig').julials.setup {
+            capabilities = capabilities,
+          }
+        end
+      end
       local servers = {
         -- clangd = {},
         -- gopls = {},
